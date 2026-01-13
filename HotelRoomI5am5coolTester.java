70e1f16874a0658e0d822e80128c29259b29c640
baraksu <baraksu@gmail.com>
2026-01-13 23:17:37 +0200

    Sync HotelRoomI5am5coolTester.java from maman02-i5am5cool

---
+++ HotelRoomI5am5coolTester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelRoomI5am5coolTester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
